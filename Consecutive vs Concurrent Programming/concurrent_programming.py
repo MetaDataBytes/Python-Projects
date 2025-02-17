18ce460a1970e0dba4c668618b84cc79b66f8e8f
Justin Osborne <JustinOsborne96@yahoo.com>
2025-02-17 08:51:38 -0600

    Add files via upload

---
+++ Consecutive vs Concurrent Programming/concurrent_programming.py
@@ -0,0 +1,8 @@
+from functions import build_list_of_team_members, pre_async_request, print_member_registration, get_time_counter
+import asyncio
+
+team = build_list_of_team_members(count=int(input("Enter the number of members to register: ")))
+start = get_time_counter()
+team = asyncio.run(main=pre_async_request(team=team))
+end = get_time_counter()
+print_member_registration(start=start, end=end, team=team)
---
+++ Consecutive vs Concurrent Programming/functions.py
@@ -0,0 +1,59 @@
+from aiolimiter import AsyncLimiter
+import aiohttp
+import json
+import random as rand
+import requests
+import time
+
+def build_list_of_team_members(count: int) -> list:
+    """
+    PURPOSE: Build a random list, of size count, of names and job titles\n
+    ARGUMENT: count (int): The number of members to make in the list\n
+    RETURN: (list): A list of random names and job titles
+    """
+    names = ["justin", "jason", "amanda", "tracy", "destiny", "catherine", "robert", "hazel", "dillion", "matthew"]
+    jobs = ["data analyst", "data engineer", "data scientist", "software developer", "software engineer", "leader", "product owner", "ai engineer", "ai developer", "solution architect"]
+    return [{"name": names[rand.randint(a=0, b=len(names) - 1)], "job": jobs[rand.randint(a=0, b=len(jobs) - 1)]} for i in range(count)]
+
+def get_time_counter() -> float:
+    """
+    PURPOSE: Get a float representing the current time\n
+    ARGUMENT: None\n
+    RETURN: (float): representation of current time\n
+    """
+    return time.perf_counter()
+
+def print_member_registration(start: float, end: float, team: list) -> None:
+    """
+    PURPOSE: print the member registration confirmations\n
+    ARGUMENT: start (float): The start time of the request, end (float): the end time of the request, team (list): a list of api response from api\n
+    RETURN: None\n
+    """
+    print(f"Total run time for processing {len(team)} team members is: {end - start}")
+    for member in team: print(member["form"])
+
+def make_request(member: dict) -> json:
+    """
+    PURPOSE: Make request to api\n
+    ARGUMENT: team (list): The team to register\n
+    RETURN: (json): response from api confirming member registration\n
+    """
+    return requests.post(url="https://httpbin.org/post", data=member).json()
+
+async def pre_async_request(team: list) -> list:
+    """
+    PURPOSE: prep for async api request\n
+    ARGUMENT: team (list): a list of names and job titles\n
+    RETURN: (list): responses from api request\n
+    """
+    async with aiohttp.ClientSession() as session: 
+        return [await async_request(session=session, member=member) for member in team]
+
+async def async_request(session: aiohttp.ClientSession, member: dict) -> json:
+    """
+    PURPOSE: Make async request to api\n
+    ARGUMENT: session (ClientSession), person: (dict)\n
+    RETURN: (json): response from api confirming member registration\n
+    """
+    async with AsyncLimiter(max_rate=10, time_period=60): 
+        async with session.post(url="https://httpbin.org/post", data=member) as response: return await response.json()
