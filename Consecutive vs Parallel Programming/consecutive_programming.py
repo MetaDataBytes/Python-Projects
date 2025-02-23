2090821a0b113e9ff15cfec746b9b634a44a18bd
Justin Osborne <JustinOsborne96@yahoo.com>
2025-02-22 21:55:58 -0600

    Delete Consecutive vs Parallel Programming directory

--- Consecutive vs Parallel Programming/consecutive_programming.py
+++
@@ -1,8 +0,0 @@
-from functions import get_list_of_numbers, get_time_counter, count, print_results
-
-if __name__ == "__main__":
-    numbers = get_list_of_numbers(int(input("Size of list: ")))
-    start = get_time_counter()
-    results = [count(number=number) for number in numbers]
-    end = get_time_counter()
-    print_results(results=results, start=start, end=end)
