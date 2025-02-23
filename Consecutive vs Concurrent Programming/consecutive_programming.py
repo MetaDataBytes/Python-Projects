bfa0290f7e1c61bfe65adfefae4a23ad0ffc74bf
Justin Osborne <JustinOsborne96@yahoo.com>
2025-02-22 21:56:30 -0600

    Add files via upload

---
+++ Consecutive vs Concurrent Programming/consecutive_programming.py
@@ -0,0 +1,8 @@
+from functions import build_list_of_team_members, make_request, print_member_registration, get_time_counter
+
+if __name__ == "__main__":
+    team = build_list_of_team_members(count=int(input("Enter the number of members to register: ")))
+    start = get_time_counter()
+    team = [make_request(member=member) for member in team]
+    end = get_time_counter()
+    print_member_registration(start=start, end=end, team=team)
