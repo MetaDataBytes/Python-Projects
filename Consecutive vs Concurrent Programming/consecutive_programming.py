4c120cab0f4d4348729a2d1fe9062a2959048775
Justin Osborne <JustinOsborne96@yahoo.com>
2025-02-22 21:55:47 -0600

    Delete Consecutive vs Concurrent Programming directory

--- Consecutive vs Concurrent Programming/consecutive_programming.py
+++
@@ -1,7 +0,0 @@
-from functions import build_list_of_team_members, make_request, print_member_registration, get_time_counter
-
-team = build_list_of_team_members(count=int(input("Enter the number of members to register: ")))
-start = get_time_counter()
-team = [make_request(member=member) for member in team]
-end = get_time_counter()
-print_member_registration(start=start, end=end, team=team)
