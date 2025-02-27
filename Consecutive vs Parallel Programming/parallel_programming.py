a083c50512f8b5ee9e1f0714c03a30fb1fc69d42
Justin Osborne <JC_Osborne@outlook.com>
2025-02-26 22:13:42 -0600

    Add files via upload

--- Consecutive vs Parallel Programming/parallel_programming.py
+++ Consecutive vs Parallel Programming/parallel_programming.py
@@ -1,9 +1,7 @@
-from functions import get_list_of_numbers, get_time_counter, count, print_results
+from functions import get_list_of_numbers, count
 import multiprocessing
 
 if __name__ == "__main__":
     numbers = get_list_of_numbers(int(input("Size of list: ")))
-    start = get_time_counter()
     results = multiprocessing.Pool().map(count, numbers)
-    end = get_time_counter()
-    print_results(results=results, start=start, end=end)
+    print(results)
