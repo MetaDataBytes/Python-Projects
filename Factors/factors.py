8571b62c6b9e2fcace94c23d7a4e9e130f3c0baa
Justin Osborne <JC_Osborne@outlook.com>
2025-03-05 08:39:18 -0600

    Add files via upload

--- Factors/factors.py
+++ Factors/factors.py
@@ -2,5 +2,5 @@ from functions import get_factor_pairs_list, print_factors
 
 if __name__ == "__main__":
     num = 1_000_000_000_000_000
-    factors = [i for i in get_factor_pairs_list(num=num)]
+    factors = get_factor_pairs_list(num=num)
     print_factors(factors=factors)
